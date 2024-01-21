90528a00a5e53049efa50528cea593e3885656b4
dennisikechukwu <155369792+dennisikechukwu@users.noreply.github.com>
2024-01-21 20:56:18 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,46 @@
+const selectElement = selector => {
+    const element = document.querySelector(selector)
+    if(element) return element;
+    throw new Error("something went wrong, make sure that $(selector) exists or is typed correctly.");
+};
+
+
+const scrollHeader = () => {
+    const headerElement = selectElement("#header");
+    if(this.scrollY >= 15){
+        headerElement.classList.add("activated");
+    }else{
+        headerElement.classList.remove("activated");
+    }
+};
+
+window.addEventListener("scroll", scrollHeader);
+
+
+const menuToggleIcon = selectElement("#menu-toggle-icon");
+
+const toggleMenu = () => {
+    const mobileMenu = selectElement("#menu");
+    mobileMenu.classList.toggle("activated");
+    menuToggleIcon.classList.toggle("activated");
+};
+
+menuToggleIcon.addEventListener("click", toggleMenu);
+
+const bodyElement = document.body;
+const themeToggleBtn = document.querySelector("#theme-toggle-btn");
+const currentTheme = localStorage.getItem("currentTheme");
+
+if(currentTheme){
+    bodyElement.classList.add("light-theme");
+}
+
+themeToggleBtn.addEventListener("click", () => {
+    bodyElement.classList.toggle("light-theme");
+
+    if(bodyElement.classList.contains("light-theme")){
+        localStorage.setItem("currentTheme", "themeActive");
+    }else{
+        localStorage.removeItem("currentTheme");
+    }
+});
